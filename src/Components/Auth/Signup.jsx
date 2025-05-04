d5cf12b446858bd71d2d690ad44584de48e5f7ad
unknown <shubhgupta4102@gmail.com>
2025-05-04 11:04:54 +0530

    bug fixed

--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -126,7 +126,7 @@ export default function SignupForm() {
       </div>
 
       {loading && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0  bg-opacity-50 flex items-center justify-center z-50">
           <div className="w-16 h-16 border-8 border-gray-300 border-t-[#003E8A] rounded-full animate-spin"></div>
         </div>
       )}
