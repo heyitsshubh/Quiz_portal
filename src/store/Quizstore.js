c1a232a29123b3078ca4132fcfb7942df9ee4327
unknown <shubhgupta4102@gmail.com>
2025-07-18 01:00:51 +0530

    added zustand store for question submission

---
+++ src/store/Quizstore.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useQuizStore = create(persist(
+  (set) => ({
+    questionIndex: 0,
+    userAnswers: [],
+    timeLeft: 0,
+    timeSinceStart: 0,
+    tabSwitchCount: 0,
+    selectedOption: null,
+
+    setQuestionIndex: (index) => set({ questionIndex: index }),
+    setUserAnswers: (answers) => set({ userAnswers: answers }),
+    setTimeLeft: (time) => set({ timeLeft: Number(time) || 0 }),
+    setTimeSinceStart: (time) => set({ timeSinceStart: Number(time) || 0 }),
+    incrementTabSwitch: () => set((state) => ({ tabSwitchCount: state.tabSwitchCount + 1 })),
+    setSelectedOption: (option) => set({ selectedOption: option }),
+  }),
+  {
+    name: 'quiz-store',
+  }
+));
