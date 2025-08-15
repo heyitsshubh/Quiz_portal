0f34a789808045aef24000323aabdf8de540fa93
unknown <shubhgupta4102@gmail.com>
2025-08-15 12:57:28 +0530

    added image

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -21,26 +21,33 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <QuizProvider>
-    <Router>
-      <ToastContainer position="top-center" autoClose={3000} />
-      <Routes>
-        <Route path="/signin" element={<Login />} />
-        <Route path="/" element={<SignupForm />} />
-        <Route path="/forgot" element={<Forgot />} />
-        <Route path="/userdashboard" element={<Userdashboard />} />
-        <Route path="/quiz/:quizId/:questionIndex" element={<QuizQuestion />} />
-        <Route path="quiz-submission-success" element={<QuizSubmissionSuccess />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/dashboard/*" element={<AdminLayout />}>
-        <Route index element={<Admindashboard />} /> 
-        <Route path="quiz-creator" element={<QuizCreator />} />
-        <Route path="add-questions" element={<AddQuestions />} />
-        <Route path="view-test" element={<ViewTest />} />
-        <Route path="results-dashboard" element={<Resultdashboard/>} />                
-        <Route path="results/:quizId" element={<Results />} />
-        </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <ToastContainer
+          position="top-center"
+          autoClose={2000}
+          toastClassName={() =>
+            "bg-[#003E8A] text-white font-semibold rounded-lg shadow-lg"
+          }
+          bodyClassName={() => "text-white text-base"}
+        />
+        <Routes>
+          <Route path="/signin" element={<Login />} />
+          <Route path="/" element={<SignupForm />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/userdashboard" element={<Userdashboard />} />
+          <Route path="/quiz/:quizId/:questionIndex" element={<QuizQuestion />} />
+          <Route path="quiz-submission-success" element={<QuizSubmissionSuccess />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/dashboard/*" element={<AdminLayout />}> 
+            <Route index element={<Admindashboard />} /> 
+            <Route path="quiz-creator" element={<QuizCreator />} />
+            <Route path="add-questions" element={<AddQuestions />} />
+            <Route path="view-test" element={<ViewTest />} />
+            <Route path="results-dashboard" element={<Resultdashboard/>} />                
+            <Route path="results/:quizId" element={<Results />} />
+          </Route>
+        </Routes>
+      </Router>
     </QuizProvider>
   );
 }
